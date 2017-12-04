c5330db8e0c612fb53acf375a3d56c3031f12450
Moreno La Quatra <moreno.laquatra@gmail.com>
2017-12-04 21:31:14 +0100

    script to generate custom artificial data
    The custom script is dedicated to the creation of a pseudo-random and
    parametrisable test set

---
+++ test/generate_dataset.py
@@ -0,0 +1,28 @@
+# encoding utf-8
+import random
+
+MAX_ID = 2000;
+MAX_ITEMS = 100;
+N_TRASACTIONS = 1000000
+
+file_csv = open('./data/artificial_dataset_pfp.csv','w')
+file_tsv = open('./data/artificial_dataset_pfp.tsv','w')
+
+for i in range(0, N_TRASACTIONS):
+    n_trans = random.sample(range(1, MAX_ITEMS), 1)[0]
+    list_trans = random.sample(range(1, MAX_ID), n_trans)
+    csv_string = str(i)
+    tsv_string = str(i)
+    for num in list_trans:
+        csv_string = csv_string + ',' + str(num)
+        tsv_string = tsv_string + '\t' + str(num)
+
+    csv_string = csv_string + '\n'
+    tsv_string = tsv_string + '\n'
+    print i;
+
+    file_csv.write(csv_string)
+    file_tsv.write(tsv_string)
+
+file_csv.close()
+file_tsv.close()
