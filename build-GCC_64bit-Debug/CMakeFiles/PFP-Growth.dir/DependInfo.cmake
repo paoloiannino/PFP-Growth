9e36560f6d7ee5cf765a013d29c109aa49e43ce9
Paolo Iannino <ianninop@gmail.com>
2017-12-04 14:56:24 +0100

    Added debug build, test folder, data folder, datasets

---
+++ build-GCC_64bit-Debug/CMakeFiles/PFP-Growth.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/paolo/Documents/HPC/PFP-Growth/src/fp_tree.cpp" "/home/paolo/Documents/HPC/PFP-Growth/build-GCC_64bit-Debug/CMakeFiles/PFP-Growth.dir/src/fp_tree.cpp.o"
+  "/home/paolo/Documents/HPC/PFP-Growth/src/main.cpp" "/home/paolo/Documents/HPC/PFP-Growth/build-GCC_64bit-Debug/CMakeFiles/PFP-Growth.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-GCC_64bit-Debug/CMakeFiles/PFP-Growth.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PFP-Growth.dir/src/main.cpp.o"
+  "CMakeFiles/PFP-Growth.dir/src/fp_tree.cpp.o"
+  "PFP-Growth.pdb"
+  "PFP-Growth"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PFP-Growth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
